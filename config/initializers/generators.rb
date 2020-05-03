4d8af20f78a29da8843d0e4bacdde2c64da7fc8c
Subhash Chandra <tmayad+github@gmail.com>
2020-05-03 19:04:13 +0530

    Configured generators

---
+++ config/initializers/generators.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+Rails.application.config.generators do |generate|
+  generate.helper false
+  generate.javascript_engine nil
+  generate.stylesheets false
+  generate.view_specs false
+end
